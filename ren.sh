5995e15a258acc6304a85f35f4f2ec3a53d611d7
dimo <forker4@dim0.uu.me>
2024-03-07 20:59:41 +0800

    try json

--- ren.sh
+++ ren.sh
@@ -2,7 +2,6 @@ cd "$1"
 for file in *.html; do
     filename=$(basename "$file")
     # 读取文件名并将其添加到文件的第一行
-    sed -i "1i ---" "$file"
-    sed -i "1i title: $filename" "$file"
-    sed -i "1i ---" "$file"
+    sed -i "1i ;;;" "$file"
+    sed -i "1s|^|{\"title\": \"$filename\"}\n|" "$file"
 done
